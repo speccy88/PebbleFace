a6b517a5b0e1aa0adf6843fd476788b4e8253b31
Frederick Blais <fred_blais5@hotmail.com>
2015-12-15 14:47:36 -0500

    implement omega.py

--- server/omega.py
+++ server/omega.py
@@ -1,1 +1,34 @@
+from subprocess import call
+from bottle import *
 
+GREEN = 16
+BLUE = 15
+RED = 17
+PINS = (RED, GREEN, BLUE)
+HI = 0
+LO = 1
+
+def state(pin, state):
+    if pin:
+        call(["fast-gpio","set", str(pin), state])
+    else:
+        for pin in PINS:
+            call(["fast-gpio","set", str(pin), state])
+
+def off(pin=None):
+    state(pin, str(LO))
+
+def on(pin=None):
+    state(pin, str(HI))
+
+@post('/')
+def toggle():
+    state = request.forms.get("state")
+    if state=="ON":
+        print("ON")
+        on()
+    else:
+        print("OFF")
+        off()
+
+run(host="0.0.0.0", port=5000)
